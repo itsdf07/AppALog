482cbcf567a168546caee7f38e555e5b387fb5b5
itsdf07 <itsdf07@gmail.com>
2020-01-08 13:18:36 +0800

    增加ALog打印功能

--- app/src/main/java/com/itsdf07/app/alog/MainActivity.java
+++ app/src/main/java/com/itsdf07/app/alog/MainActivity.java
@@ -3,11 +3,14 @@ package com.itsdf07.app.alog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.itsdf07.lib.alog.ALog;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ALog.dTag("itsdf07","测试Log");
     }
 }
---
+++ libalog/src/main/java/com/itsdf07/lib/alog/ALogLevel.java
@@ -0,0 +1,21 @@
+package com.itsdf07.lib.alog;
+
+/**
+ * @Description ：
+ * @Author itsdf07
+ * @E-Mail 923255742@qq.com
+ * @Github https://github.com/itsdf07
+ * @Date 2020/1/8
+ */
+
+public enum ALogLevel {
+    /**
+     * 打印日志
+     */
+    FULL,
+
+    /**
+     * 关闭日志
+     */
+    NONE
+}

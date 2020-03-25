1325c16c2180c75050790e57ebe701e318fff481
itsdf07 <923255742@qq.com>
2020-03-25 11:21:41 +0800

    增加UI界面调用ALog工具

--- app/src/main/java/com/itsdf07/app/alog/MainActivity.java
+++ app/src/main/java/com/itsdf07/app/alog/MainActivity.java
@@ -1,25 +1,58 @@
 package com.itsdf07.app.alog;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import com.itsdf07.lib.alog.ALog;
 import com.itsdf07.lib.alog.ALogLevel;
 
 public class MainActivity extends AppCompatActivity {
+    private static final int REQUEST_EXTERNAL_STORAGE = 1;
+    private static String[] PERMISSIONS_STORAGE = {
+            "android.permission.READ_EXTERNAL_STORAGE",
+            "android.permission.WRITE_EXTERNAL_STORAGE"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        ALog.init()
-                .setLogLevel(ALogLevel.FULL) //是否打印log
-                .setTag("itsdf07") //自定义tag
-                .setDefineALogFilePath("xxx/xxx/xxx.log") //自定义log存储路径
-                .setLog2Local(true) //设置是否本地存储log记录
-                .setShowThreadInfo(false)//是否显示线程信息
-                .setMethodCount(2); //显示函数栈中的方法数
         setContentView(R.layout.activity_main);
-ALog.d("这是第 %s 次 Log 输出", 6);
-ALog.dTag("aso", "这是第 %s 次 Log 输出", 7);
+        findViewById(R.id.btnLogTest).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkPermission();
+            }
+        });
+
+    }
+
+    private void checkPermission() {
+        //检查权限（NEED_PERMISSION）是否被授权 PackageManager.PERMISSION_GRANTED表示同意授权
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            //用户已经拒绝过一次，再次弹出权限申请对话框需要给用户一个解释
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission
+                    .WRITE_EXTERNAL_STORAGE)) {
+                Toast.makeText(this, "请开通相关权限，否则无法正常使用本应用文件存储功能！", Toast.LENGTH_SHORT).show();
+                ALog.v("请开通相关权限，否则无法正常使用本应用文件存储功能！");
+            }
+            //申请权限
+            ActivityCompat.requestPermissions(this, PERMISSIONS_STORAGE, REQUEST_EXTERNAL_STORAGE);
+
+        } else {
+            Toast.makeText(this, "授权成功！", Toast.LENGTH_SHORT).show();
+            ALog.v("文件读写权限已授权！");
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        ALog.v("requestCode:%s,resultCode:%s,data:%s", requestCode, resultCode, data);
     }
 }
